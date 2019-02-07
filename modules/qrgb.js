88290e58020d73f960eb619723ac04b2438f6dad
russell elfenbein <russell.elfenbein@gmail.com>
2019-02-07 22:16:17 +0000

    faster

--- modules/qrgb.js
+++ modules/qrgb.js
@@ -1,11 +1,11 @@
 module.exports = function(){
     function generate(text){
         return new Promise((resolve,reject)=>{
-            const none = '00000000';
+            // const none = '00000000';
             let buff = new Array(text.length);
             for(let i = 0; i < text.length; i++){
                 let tmp = text.charCodeAt(i).toString(2);
-                buff[i] = none.substring(tmp.length) + tmp;
+                buff[i] = tmp;//none.substring(tmp.length) + tmp;
             }
             resolve(buff);
         })
